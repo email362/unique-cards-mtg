f2688cae1844403551124468a2987c24d10542b8
Chris Mac <hyer.chris@gmail.com>
2021-02-22 08:57:01 -0800

    added an initial package.json file and converted most of the code to a promise/then chain

--- index.js
+++ index.js
@@ -7,9 +7,13 @@
 // -------------------------------------------------------
 // 1: Read initial text file list
 // 2: Remove duplicate names
-// 3: Write remaining names on new text file list
-const fs = require('fs')
-const readline = require('readline')
+// 3: Save remaining to an array
+// 4: Sort the array
+// 5: Write remaining names on new text file list
+import fs from 'fs';
+import readline from 'readline';
+
+import {readFile, writeFile} from 'fs/promises';
 
 const readfile = './list.txt'
 
@@ -53,9 +57,52 @@ async function lineByLine() {
 
 }
 
+function readInput(someTxtFile) {
+    return readFile(someTxtFile,'utf-8');
+}
+
+function createArrOfLines(txtDoc) {
+    return new Promise((resolve,reject) => {
+        if(typeof txtDoc === 'string') {
+            resolve(txtDoc.split('\n'));
+        } else {
+            reject('not a valid string value');
+        }
+    })
+}
+
+function sortArr(arr) {
+    return new Promise((resolve, reject) => {
+        if(arr === undefined) {
+            return reject('nothing defined! no array to sort!')
+        }
+        resolve(arr.sort())
+    })
+}
+
+function removeDuplicates(arr) {
+    return new Promise((resolve, reject) => {
+        if(arr === undefined ) { return reject('undefined')}
+
+        const uniqueArr = arr.filter((item, i, calledArr)=> {
+            if(i === 0) {
+                return true;
+            } else {
+                const arrOfPrev = calledArr.slice(i-1,i);
+
+                return !arrOfPrev.includes(item);
+            }
 
 
+        })
 
-lineByLine()
+        resolve(uniqueArr);
+
+    })
+}
+
+// lineByLine()
+
+readInput('list.txt').then(data => createArrOfLines(data)).then(data2 => sortArr(data2)).then(sorted => removeDuplicates(sorted)).then(unique => console.log(unique));
 
 
